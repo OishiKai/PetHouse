08744312bc00dd05ef81717876055ea788dd442d
oishikai <bokuchan.kai@gmail.com>
2021-12-22 03:05:51 +0900

    里親用アンケートテーブル作成

--- database/migrations/2021_12_21_172451_create_fosterquestionnaires_table.php
+++ database/migrations/2021_12_21_172451_create_fosterquestionnaires_table.php
@@ -14,12 +14,24 @@ class CreateFosterquestionnairesTable extends Migration
     public function up()
     {
         Schema::create('fosterquestionnaires', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->integer('user_email')->unique();
+            $table->string('housemate')->default('none'); // あり(家族) or あり(家族以外) or なし
+            $table->integer('housemateNumber')->default('0');
+            $table->string('housemateDetail')->default('none');// 続柄=>〇〇,年齢=>〇歳&続柄=>〇〇,年齢=>〇歳&... で一つの文字列にする
+            $table->string('housemateAgreement')->default('none');
+            $table->string('housemateAllergy')->default('none');// 人数 or なし
+            $table->string('housemateAllergyDetail')->default('none');// 続柄=>〇〇,アレルギー=>〇&続柄=>〇〇,アレルギー=>〇&... で一つの文字列にする
+            $table->string('houseType')->default('none');
+            $table->string('landlordAgreement')->default('none');
+            $table->string('transfer')->default('none');
+            $table->string('visitable')->default('none');
+            $table->string('breedingPlace')->default('none');
+            $table->integer('aloneHours')->default('0');
+            $table->string('situation')->default('none');
         });
     }
 
-    /**
+    /**situation
      * Reverse the migrations.
      *
      * @return void
