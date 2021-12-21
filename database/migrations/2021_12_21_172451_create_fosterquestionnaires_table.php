cf5a4dea062596ad10b59831542db2176740c80b
oishikai <bokuchan.kai@gmail.com>
2021-12-22 02:25:34 +0900

    里親用のアンケートテーブルに変更

--- database/migrations/2021_12_21_172451_create_fosterquestionnaires_table.php
+++ database/migrations/2021_12_21_172451_create_fosterquestionnaires_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionnairesTable extends Migration
+class CreateFosterquestionnairesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateQuestionnairesTable extends Migration
      */
     public function up()
     {
-        Schema::create('questionnaires', function (Blueprint $table) {
+        Schema::create('fosterquestionnaires', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateQuestionnairesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('questionnaires');
+        Schema::dropIfExists('fosterquestionnaires');
     }
 }
