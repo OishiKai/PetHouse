27fcaa64e27214bebc1abe582dda5f57c806d5dd
oishikai <bokuchan.kai@gmail.com>
2021-12-26 03:54:46 +0900

    formSwitch()を改良

--- public/js/formSwitch.js
+++ public/js/formSwitch.js
@@ -1,13 +1,26 @@
 function formSwitch() {
     hoge = document.getElementsByName('switch')
-    if (hoge[0].checked) {
-        // 好きな食べ物が選択されたら下記を実行します
-        document.getElementById('default').style.display = "";
-        document.getElementById('another').style.display = "none";
-    } else if (hoge[1].checked) {
-        // 好きな場所が選択されたら下記を実行します
-        document.getElementById('default').style.display = "none";
-        document.getElementById('another').style.display = "";
+    console.log(hoge.length);
+    if (hoge.length == 1){
+        if (hoge[0].checked) {
+            // 好きな食べ物が選択されたら下記を実行します
+            document.getElementById('default').style.display = "";
+            document.getElementById('another').style.display = "none";
+        } else{
+            // 好きな場所が選択されたら下記を実行します
+            document.getElementById('default').style.display = "none";
+            document.getElementById('another').style.display = "";
+        }
+    }else{
+        if (hoge[0].checked) {
+            // 好きな食べ物が選択されたら下記を実行します
+            document.getElementById('default').style.display = "";
+            document.getElementById('another').style.display = "none";
+        } else if (hoge[1].checked) {
+            // 好きな場所が選択されたら下記を実行します
+            document.getElementById('default').style.display = "none";
+            document.getElementById('another').style.display = "";
+        }
     }
 }
 window.addEventListener('load', formSwitch());
