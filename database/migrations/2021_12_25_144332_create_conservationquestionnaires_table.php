3e039da5ae39c2a48adde7e73e1f6f4cc8856f25
oishikai <bokuchan.kai@gmail.com>
2021-12-26 00:55:37 +0900

    テーブルにデフォルト値を設定

--- database/migrations/2021_12_25_144332_create_conservationquestionnaires_table.php
+++ database/migrations/2021_12_25_144332_create_conservationquestionnaires_table.php
@@ -14,18 +14,18 @@ class CreateConservationquestionnairesTable extends Migration
     public function up()
     {
         Schema::create('conservationquestionnaires', function (Blueprint $table) {
-            $table->string('user_email');
-            $table->string('answered');
-            $table->string('conservationStatus');
-            $table->string('activityName');
-            $table->string('address');
-            $table->string('postalCode');
-            $table->string('shelter');
-            $table->string('pet'); // 保護中のペット
-            $table->string('area');
-            $table->string('url');
-            $table->string('profile');
-            $table->string('logo');
+            $table->string('user_email')->unique();
+            $table->string('answered')->default('none');
+            $table->string('conservationStatus')->default('none');
+            $table->string('activityName')->default('none');
+            $table->string('address')->default('none');
+            $table->string('postalCode')->default('none');
+            $table->string('shelter')->default('none');
+            $table->string('pet')->default('none'); // 保護中のペット
+            $table->string('area')->default('none');
+            $table->string('url')->default('none');
+            $table->string('profile')->default('none');
+            $table->string('logo')->default('none');
         });
     }
 
