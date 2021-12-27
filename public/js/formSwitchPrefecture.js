55c553e3551dbfcdb8a9bb127d2370614af01773
oishikai <bokuchan.kai@gmail.com>
2021-12-27 23:33:32 +0900

    不要コード削除

--- public/js/formSwitchPrefecture.js
+++ public/js/formSwitchPrefecture.js
@@ -9,27 +9,5 @@ function formSwitchPrefecture(state) {
     } else{
         document.getElementById(pre).style.display = "none";
     }
-
-    // if (hoge.length == 1){
-    //     if (hoge[0].checked) {
-    //         // 好きな食べ物が選択されたら下記を実行します
-    //         document.getElementById('default').style.display = "";
-    //         document.getElementById('another').style.display = "none";
-    //     } else{
-    //         // 好きな場所が選択されたら下記を実行します
-    //         document.getElementById('default').style.display = "none";
-    //         document.getElementById('another').style.display = "";
-    //     }
-    // }else{
-    //     if (hoge[0].checked) {
-    //         // 好きな食べ物が選択されたら下記を実行します
-    //         document.getElementById('default').style.display = "";
-    //         document.getElementById('another').style.display = "none";
-    //     } else if (hoge[1].checked) {
-    //         // 好きな場所が選択されたら下記を実行します
-    //         document.getElementById('default').style.display = "none";
-    //         document.getElementById('another').style.display = "";
-    //     }
-    // }
 }
 window.addEventListener('load', formSwitchPrefecture());
