fbff1c21005058533326dc05432cb3c7f6700695
oishikai <bokuchan.kai@gmail.com>
2022-01-28 10:37:09 +0900

    都道府県のデータ受け渡しを調整

--- resources/views/conservationQuestionnaire.blade.php
+++ resources/views/conservationQuestionnaire.blade.php
@@ -2,6 +2,7 @@
 
 @section('head')
 <script src="{{ asset('js/conservationQuestionnaireScript.js') }}"></script>
+<script src="{{ asset('js/formSwitchPrefecture.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('css/conservationQuestionnaireStyle.css') }}">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
@@ -123,90 +124,90 @@
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="東北地方">東北地方</label></dt>
+                            <dt><label><input type="checkbox" value="東北地方" id='tohoku' onchange='formSwitchPrefecture("tohoku")'>東北地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="青森県">青森県</label>
-                                <label><input type="checkbox" name="area[]" value="岩手県">岩手県</label>
-                                <label><input type="checkbox" name="area[]" value="宮城県">宮城県</label>
-                                <label><input type="checkbox" name="area[]" value="秋田県">秋田県</label>
-                                <label><input type="checkbox" name="area[]" value="山形県">山形県</label>
-                                <label><input type="checkbox" name="area[]" value="福島県">福島県</label>
+                                <label><input type="checkbox" name="area[]" value="青森県" id='tohokuChild1' onchange='formSwitchPrefecture("tohokuChild")' >青森県</label>
+                                <label><input type="checkbox" name="area[]" value="岩手県" id='tohokuChild2' onchange='formSwitchPrefecture("tohokuChild")' >岩手県</label>
+                                <label><input type="checkbox" name="area[]" value="宮城県" id='tohokuChild3' onchange='formSwitchPrefecture("tohokuChild")' >宮城県</label>
+                                <label><input type="checkbox" name="area[]" value="秋田県" id='tohokuChild4' onchange='formSwitchPrefecture("tohokuChild")' >秋田県</label>
+                                <label><input type="checkbox" name="area[]" value="山形県" id='tohokuChild5' onchange='formSwitchPrefecture("tohokuChild")' >山形県</label>
+                                <label><input type="checkbox" name="area[]" value="福島県" id='tohokuChild6' onchange='formSwitchPrefecture("tohokuChild")' >福島県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="関東地方">関東地方</label></dt>
+                            <dt><label><input type="checkbox" value="関東地方" id='kanto' onchange='formSwitchPrefecture("kanto")'>関東地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="茨城県">茨城県</label>
-                                <label><input type="checkbox" name="area[]" value="栃木県">栃木県</label>
-                                <label><input type="checkbox" name="area[]" value="群馬県">群馬県</label>
-                                <label><input type="checkbox" name="area[]" value="埼玉県">埼玉県</label>
-                                <label><input type="checkbox" name="area[]" value="千葉県">千葉県</label>
-                                <label><input type="checkbox" name="area[]" value="東京都">東京都</label>
-                                <label><input type="checkbox" name="area[]" value="神奈川県">神奈川県</label>
+                                <label><input type="checkbox" name="area[]" value="茨城県" id='kantoChild1' onchange='formSwitchPrefecture("kantoChild")'>茨城県</label>
+                                <label><input type="checkbox" name="area[]" value="栃木県" id='kantoChild2' onchange='formSwitchPrefecture("kantoChild")'>栃木県</label>
+                                <label><input type="checkbox" name="area[]" value="群馬県" id='kantoChild3' onchange='formSwitchPrefecture("kantoChild")'> 群馬県</label>
+                                <label><input type="checkbox" name="area[]" value="埼玉県" id='kantoChild4' onchange='formSwitchPrefecture("kantoChild")'>埼玉県</label>
+                                <label><input type="checkbox" name="area[]" value="千葉県" id='kantoChild5' onchange='formSwitchPrefecture("kantoChild")'>千葉県</label>
+                                <label><input type="checkbox" name="area[]" value="東京都" id='kantoChild6' onchange='formSwitchPrefecture("kantoChild")'>東京都</label>
+                                <label><input type="checkbox" name="area[]" value="神奈川県" id='kantoChild7' onchange='formSwitchPrefecture("kantoChild")'>神奈川県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="中部地方">中部地方</label></dt>
+                            <dt><label><input type="checkbox" value="中部地方" id='tyubu' onchange='formSwitchPrefecture("tyubu")'>中部地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="新潟県">新潟県</label>
-                                <label><input type="checkbox" name="area[]" value="富山県">富山県</label>
-                                <label><input type="checkbox" name="area[]" value="石川県">石川県</label>
-                                <label><input type="checkbox" name="area[]" value="福井県">福井県</label>
-                                <label><input type="checkbox" name="area[]" value="山梨県">山梨県</label>
-                                <label><input type="checkbox" name="area[]" value="長野県">長野県</label>
-                                <label><input type="checkbox" name="area[]" value="岐阜県">岐阜県</label>
-                                <label><input type="checkbox" name="area[]" value="静岡県">静岡県</label>
-                                <label><input type="checkbox" name="area[]" value="愛知県">愛知県</label>
+                                <label><input type="checkbox" name="area[]" value="新潟県" id='tyubuChild1' onchange='formSwitchPrefecture("tyubuChild")'>新潟県</label>
+                                <label><input type="checkbox" name="area[]" value="富山県" id='tyubuChild2' onchange='formSwitchPrefecture("tyubuChild")'>富山県</label>
+                                <label><input type="checkbox" name="area[]" value="石川県" id='tyubuChild3' onchange='formSwitchPrefecture("tyubuChild")'>石川県</label>
+                                <label><input type="checkbox" name="area[]" value="福井県" id='tyubuChild4' onchange='formSwitchPrefecture("tyubuChild")'>福井県</label>
+                                <label><input type="checkbox" name="area[]" value="山梨県" id='tyubuChild5' onchange='formSwitchPrefecture("tyubuChild")'>山梨県</label>
+                                <label><input type="checkbox" name="area[]" value="長野県" id='tyubuChild6' onchange='formSwitchPrefecture("tyubuChild")'>長野県</label>
+                                <label><input type="checkbox" name="area[]" value="岐阜県" id='tyubuChild7' onchange='formSwitchPrefecture("tyubuChild")'>岐阜県</label>
+                                <label><input type="checkbox" name="area[]" value="静岡県" id='tyubuChild8' onchange='formSwitchPrefecture("tyubuChild")'>静岡県</label>
+                                <label><input type="checkbox" name="area[]" value="愛知県" id='tyubuChild9' onchange='formSwitchPrefecture("tyubuChild")'>愛知県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="近畿地方">近畿地方</label></dt>
+                            <dt><label><input type="checkbox" value="近畿地方" id='kinki' onchange='formSwitchPrefecture("kinki")'>近畿地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="三重県">三重県</label>
-                                <label><input type="checkbox" name="area[]" value="滋賀県">滋賀県</label>
-                                <label><input type="checkbox" name="area[]" value="京都府">京都府</label>
-                                <label><input type="checkbox" name="area[]" value="大阪府">大阪府</label>
-                                <label><input type="checkbox" name="area[]" value="兵庫県">兵庫県</label>
-                                <label><input type="checkbox" name="area[]" value="奈良県">奈良県</label>
-                                <label><input type="checkbox" name="area[]" value="和歌山県">和歌山県</label>
+                                <label><input type="checkbox" name="area[]" value="三重県" id='kinkiChild1' onchange='formSwitchPrefecture("kinkiChild")'>三重県</label>
+                                <label><input type="checkbox" name="area[]" value="滋賀県" id='kinkiChild2' onchange='formSwitchPrefecture("kinkiChild")'>滋賀県</label>
+                                <label><input type="checkbox" name="area[]" value="京都府" id='kinkiChild3' onchange='formSwitchPrefecture("kinkiChild")'>京都府</label>
+                                <label><input type="checkbox" name="area[]" value="大阪府" id='kinkiChild4' onchange='formSwitchPrefecture("kinkiChild")'>大阪府</label>
+                                <label><input type="checkbox" name="area[]" value="兵庫県" id='kinkiChild5' onchange='formSwitchPrefecture("kinkiChild")'>兵庫県</label>
+                                <label><input type="checkbox" name="area[]" value="奈良県" id='kinkiChild6' onchange='formSwitchPrefecture("kinkiChild")'>奈良県</label>
+                                <label><input type="checkbox" name="area[]" value="和歌山県" id='kinkiChild7' onchange='formSwitchPrefecture("kinkiChild")'>和歌山県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="中国地方">中国地方</label></dt>
+                            <dt><label><input type="checkbox" value="中国地方" id='tyugoku' onchange='formSwitchPrefecture("tyugoku")'>中国地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="鳥取県">鳥取県</label>
-                                <label><input type="checkbox" name="area[]" value="島根県">島根県</label>
-                                <label><input type="checkbox" name="area[]" value="岡山県">岡山県</label>
-                                <label><input type="checkbox" name="area[]" value="広島県">広島県</label>
-                                <label><input type="checkbox" name="area[]" value="山口県">山口県</label>
+                                <label><input type="checkbox" name="area[]" value="鳥取県" id='tyugokuChild1' onchange='formSwitchPrefecture("tyugokuChild")'>鳥取県</label>
+                                <label><input type="checkbox" name="area[]" value="島根県" id='tyugokuChild2' onchange='formSwitchPrefecture("tyugokuChild")'>島根県</label>
+                                <label><input type="checkbox" name="area[]" value="岡山県" id='tyugokuChild3' onchange='formSwitchPrefecture("tyugokuChild")'>岡山県</label>
+                                <label><input type="checkbox" name="area[]" value="広島県" id='tyugokuChild4' onchange='formSwitchPrefecture("tyugokuChild")'>広島県</label>
+                                <label><input type="checkbox" name="area[]" value="山口県" id='tyugokuChild5' onchange='formSwitchPrefecture("tyugokuChild")'>山口県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="四国地方">四国地方</label></dt>
+                            <dt><label><input type="checkbox" value="四国地方" id='shikoku' onchange='formSwitchPrefecture("shikoku")'>四国地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="徳島県">徳島県</label>
-                                <label><input type="checkbox" name="area[]" value="香川県">香川県</label>
-                                <label><input type="checkbox" name="area[]" value="愛媛県">愛媛県</label>
-                                <label><input type="checkbox" name="area[]" value="高知県">高知県</label>
+                                <label><input type="checkbox" name="area[]" value="徳島県" id='shikokuChild1' onchange='formSwitchPrefecture("shikokuChild")'>徳島県</label>
+                                <label><input type="checkbox" name="area[]" value="香川県" id='shikokuChild2' onchange='formSwitchPrefecture("shikokuChild")'>香川県</label>
+                                <label><input type="checkbox" name="area[]" value="愛媛県" id='shikokuChild3' onchange='formSwitchPrefecture("shikokuChild")'>愛媛県</label>
+                                <label><input type="checkbox" name="area[]" value="高知県" id='shikokuChild4' onchange='formSwitchPrefecture("shikokuChild")'>高知県</label>
                             </dd>
                         </dl>
 
                         <dl class="area">
-                            <dt><label><input type="checkbox" name="area[]" value="九州地方">九州地方</label></dt>
+                            <dt><label><input type="checkbox" value="九州地方" id='kyusyu' onchange='formSwitchPrefecture("kyusyu")'>九州地方</label></dt>
                             <dd>
-                                <label><input type="checkbox" name="area[]" value="福岡県">福岡県</label>
-                                <label><input type="checkbox" name="area[]" value="佐賀県">佐賀県</label>
-                                <label><input type="checkbox" name="area[]" value="長崎県">長崎県</label>
-                                <label><input type="checkbox" name="area[]" value="熊本県">熊本県</label>
-                                <label><input type="checkbox" name="area[]" value="大分県">大分県</label>
-                                <label><input type="checkbox" name="area[]" value="宮崎県">宮崎県</label>
-                                <label><input type="checkbox" name="area[]" value="鹿児島県">鹿児島県</label>
-                                <label><input type="checkbox" name="area[]" value="沖縄県">沖縄県</label>
+                                <label><input type="checkbox" name="area[]" value="福岡県" id='kyusyuChild1' onchange='formSwitchPrefecture("kyusyuChild")'>福岡県</label>
+                                <label><input type="checkbox" name="area[]" value="佐賀県" id='kyusyuChild2' onchange='formSwitchPrefecture("kyusyuChild")'>佐賀県</label>
+                                <label><input type="checkbox" name="area[]" value="長崎県" id='kyusyuChild3' onchange='formSwitchPrefecture("kyusyuChild")'>長崎県</label>
+                                <label><input type="checkbox" name="area[]" value="熊本県" id='kyusyuChild4' onchange='formSwitchPrefecture("kyusyuChild")'>熊本県</label>
+                                <label><input type="checkbox" name="area[]" value="大分県" id='kyusyuChild5' onchange='formSwitchPrefecture("kyusyuChild")'>大分県</label>
+                                <label><input type="checkbox" name="area[]" value="宮崎県" id='kyusyuChild6' onchange='formSwitchPrefecture("kyusyuChild")'>宮崎県</label>
+                                <label><input type="checkbox" name="area[]" value="鹿児島県" id='kyusyuChild7' onchange='formSwitchPrefecture("kyusyuChild")'>鹿児島県</label>
+                                <label><input type="checkbox" name="area[]" value="沖縄県" id='kyusyuChild8' onchange='formSwitchPrefecture("kyusyuChild")'>沖縄県</label>
                             </dd>
                         </dl>
 
