b7bfe860e3132ed29c673c6c23fcf040dc1776d4
oishikai <bokuchan.kai@gmail.com>
2022-02-02 12:16:17 +0900

    掲載した日付正常に表示

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -11,23 +11,7 @@
 <div id="a">
     <div class="a">
         <h2>よくある質問</h2>
-        <!-- <h3>カテゴリーを絞る</h3> -->
     </div>
-    <!-- <div class="b">
-        <select name="category" id="category">
-            <option value="すべて" selected>すべて</option>
-            <option value="PetHouseの利用について">PetHouseの利用について</option>
-            <option value="会員登録について">会員登録について</option>
-            <option value="里親希望する">里親希望する</option>
-            <option value="里親を募集する">里親を募集する</option>
-            <option value="トラブルについて">トラブルについて</option>
-            <option value="違反行為について">違反行為について</option>
-            <option value="サイトへのお問い合わせ">サイトへのお問い合わせ</option>
-            <option value="操作方法">操作方法</option>
-            <option value="その他">その他</option>
-        </select>
-
-    </div> -->
 
     <div class="qa-list mtd" style='margin: 0px 200px 0px 200px'>
         <dl class="qa">
